73e4dc01f3c833ae09273e957b194d25c9f663b5
matthewkarlson <mattjkarlson@gmail.com>
2022-05-17 09:19:44 +0100

    Update contract

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -19,8 +19,24 @@ contract ChainBattles is ERC721URIStorage {
 
     function generateCharacter(uint256) public returns (string memory){
         bytes memory svg = abi.encodePacked(
-            
-        );
+        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+        '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
+        '<rect width="100%" height="100%" fill="black" />',
+        '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
+        '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
+        '</svg>'
+    );
+    return string(
+        abi.encodePacked(
+            "data:image/svg+xml;base64,",
+            Base64.encode(svg)
+        )    
+    );
+    }
+
+    fucntion getLevels(uint256 tokenID) public view return(string memory){
+        uint256  levels = tokenIDtoLevels[tokenID];
+        return levels.toString();
     }
 
 }
